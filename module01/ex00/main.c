eb4f4d9af189a4acd990e37d467bc41f8694b9c1
tchalaou <tchalaou@f1br4s5.paris.42.school>
2026-04-14 18:45:36 +0200

    module01

--- module01/ex00/main.c
+++ module01/ex00/main.c
@@ -1,17 +1,19 @@
 #include <avr/io.h>
 
+void delay_500ms()
+{
+    volatile unsigned long i;
+    for (i = 0; i < 250000; i++)
+        asm volatile("nop");
+}
+
 int main()
 {
-    volatile unsigned long count = 0;
-
     DDRB |= (1 << PB1); // set PB1 as output (LED)
 
     while (1)
     {
-        if (++count >= F_CPU / 64)
-        {
-            count = 0;
-            PORTB ^= (1 << PB1); // toggle PB1 (LED ON/OFF)
-        }
+        PORTB ^= (1 << PB1); // toggle PB1 (LED ON/OFF)
+        delay_500ms();
     }
 }
